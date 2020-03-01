d5bb5f05d1d6661743a25eda03088aabcd567204
Will Brennan <willfbren@gmail.com>
2020-03-01 00:06:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,6 +8,17 @@ def map(array)
   new
 end
 
-def reduce(array)
-  
+def reduce(array, sv=nil)
+  if sv 
+    sum = sv
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length 
+    sum = yield(sum, array[i])
+    i += 1 
+  end 
+  sum
 end
